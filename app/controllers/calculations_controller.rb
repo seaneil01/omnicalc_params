bdacc19774fbca9c367be6dd81be204aa8eef464
seaneil01 <seaneil01@users.noreply.github.com>
2017-04-26 23:49:45 -0500

    1 error

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,8 +1,85 @@
 class CalculationsController < ApplicationController
-  def flex_square_5
-
-    render("calculations/flex_square_5.html.erb")
-
+  def flex_square_num
+    # Parameters: {"num"=>"5"}
+    @user_number = params["num"].to_f
+    @square = @user_number **2
+    render("calculations/flex_square_num.html.erb")
   end
 
+  def flex_square_root_num
+    @user_number = params["num"].to_f
+    @square_root = Math.sqrt(@user_number)
+    render("calculations/flex_square_root_num.html.erb")
+  end
+
+  def flex_payment
+    @apr = (params[:basis_points].to_f/100)
+    @years = params[:number_of_years].to_i
+    @principal = params[:present_value].to_f
+
+    monthly_apr = @apr/12/100
+    months = @years * 12
+
+    @monthly_payment = (monthly_apr*@principal)/(1-(1+monthly_apr)**(-months))
+    render("calculations/flex_payment.html.erb")
+  end
+
+  def flex_random
+    @user_min = params["min"].to_f
+    @user_max = params["max"].to_f
+    @rand = rand(@user_min..@user_max).to_i
+
+    render("calculations/flex_random.html.erb")
+  end
+
+
+
+  # START FORMS
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def square
+    @user_number = params[:user_number].to_f
+    @square = @user_number **2
+    render("calculations/square.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @user_number = params[:user_number].to_f
+    @square_root = Math.sqrt(@user_number)
+    render("calculations/square_root.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment_results
+    @apr = params[:user_apr].to_f
+    @years = params[:user_years].to_i
+    @principal = params[:user_prin].to_f
+
+    monthly_apr = @apr/12/100
+    months = @years * 12
+
+    @monthly_payment = (monthly_apr*@principal)/(1-(1+monthly_apr)**(-months))
+    render("calculations/payment_results.html.erb")
+  end
+
+  def random_form
+    render("calculations/random_form.html.erb")
+  end
+
+  def random_results
+    @user_min = params[:user_min].to_f
+    @user_max = params[:user_max].to_f
+    @rand = rand(@user_min..@user_max)
+    render("calculations/random_results.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  get("/flexible/square/5", {:controller => "calculations", :action => "flex_square_5"})
+  get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_num"})
+
+  get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root_num"})
+
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", {:controller => "calculations", :action => "flex_payment"})
+
+  get("/flexible/random/:min/:max", {:controller => "calculations", :action => "flex_random"})
+
+  # START FORMS
+
+  get("/square/new", {:controller => "calculations", :action => "square_form"})
+
+  get("/square/results", {:controller => "calculations", :action => "square"})
+
+  get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+
+  get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+
+  get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+
+  get("/payment/results", {:controller => "calculations", :action => "payment_results"})
+
+  get("/random/new", {:controller => "calculations", :action => "random_form"})
+
+  get("/random/results", {:controller => "calculations", :action => "random_results"})
 end
